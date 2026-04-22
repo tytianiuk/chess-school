693510906ed39a242b3580410e86f281918fcb2b
Tytianiuk Artem <tytianiuk@gmail.com>
2026-04-22 15:23:52 +0300

    feat: restrict access to students

---
+++ server/src/auth/roles.guard.ts
@@ -0,0 +1,37 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '@prisma/client';
+import { ROLES_KEY } from './roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    const hasRole = requiredRoles.includes(user.role);
+
+    if (!hasRole) {
+      throw new ForbiddenException(
+        'You do not have permission to access this resource',
+      );
+    }
+
+    return true;
+  }
+}
--- server/src/puzzles/puzzles.controller.ts
+++ server/src/puzzles/puzzles.controller.ts
@@ -12,12 +12,16 @@ import { PuzzlesService } from './puzzles.service';
 import { CreatePuzzleDto } from './dto/create-puzzle.dto';
 import { UpdatePuzzleDto } from './dto/update-puzzle.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
+import { Role } from '@prisma/client/wasm';
 
 @Controller('puzzles')
 export class PuzzlesController {
   constructor(private readonly puzzlesService: PuzzlesService) {}
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.COACH)
   @Post()
   create(@Body() createPuzzleDto: CreatePuzzleDto) {
     return this.puzzlesService.create(createPuzzleDto);
@@ -33,13 +37,15 @@ export class PuzzlesController {
     return this.puzzlesService.findOne(+id);
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.COACH)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePuzzleDto: UpdatePuzzleDto) {
     return this.puzzlesService.update(+id, updatePuzzleDto);
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.COACH)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.puzzlesService.remove(+id);
